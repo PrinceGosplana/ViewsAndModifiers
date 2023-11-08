9359c24367b91d193e3e1ec1cc0ca5bdb070dbb9
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-08 08:40:01 +0200

    project init

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by OLEKSANDR ISAIEV on 08.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
