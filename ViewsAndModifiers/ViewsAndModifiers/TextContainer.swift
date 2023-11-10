2d2211e62e5fddb2ff03f1b4474656af0ebf9572
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-10 06:23:52 +0200

    add view with texts

---
+++ ViewsAndModifiers/ViewsAndModifiers/TextContainer.swift
@@ -0,0 +1,18 @@
+//
+//  TextContainer.swift
+//  ViewsAndModifiers
+//
+//  Created by OLEKSANDR ISAIEV on 10.11.2023.
+//
+
+import SwiftUI
+
+struct TextContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextContainer()
+}
