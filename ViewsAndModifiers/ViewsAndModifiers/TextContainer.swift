8fa6ca78a8a8b8193ea734c86e508304b2b2958e
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-10 06:27:14 +0200

    add environment modifier

--- ViewsAndModifiers/ViewsAndModifiers/TextContainer.swift
+++ ViewsAndModifiers/ViewsAndModifiers/TextContainer.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct TextContainer: View {
     var body: some View {
-        Text("Hello, World!")
-        Text("Hello, World!")
-        Text("Hello, World!")
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+                .font(.largeTitle)
+            Text("Hello, World!")
+            Text("Hello, World!")
+            Text("Hello, World!")
+        }
+        .font(.title)
     }
 }
 
