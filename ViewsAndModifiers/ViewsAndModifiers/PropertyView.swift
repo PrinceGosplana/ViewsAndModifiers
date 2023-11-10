c91db15ccf9ecfd7b6b6497d4a2a42cb0e431012
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-10 06:36:17 +0200

    add new view for properties view

---
+++ ViewsAndModifiers/ViewsAndModifiers/PropertyView.swift
@@ -0,0 +1,19 @@
+//
+//  PropertyView.swift
+//  ViewsAndModifiers
+//
+//  Created by OLEKSANDR ISAIEV on 10.11.2023.
+//
+
+import SwiftUI
+
+struct PropertyView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PropertyView()
+}
