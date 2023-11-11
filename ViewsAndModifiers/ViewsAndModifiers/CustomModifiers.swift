3c9479d9b32b0e268b44d641bbcec54dc2d386a8
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-11 10:57:23 +0200

    add watermark custom view

--- ViewsAndModifiers/ViewsAndModifiers/CustomModifiers.swift
+++ ViewsAndModifiers/ViewsAndModifiers/CustomModifiers.swift
@@ -24,10 +24,34 @@ extension View {
     }
 }
 
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
 struct CustomModifiers: View {
     var body: some View {
         Text("Hello, World!")
             .titleStyle()
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hack Swift")
     }
 }
 
