36e92624e4f04325a0b2768493e90dc5e262f46b
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-11 07:20:03 +0200

    add view for custom modifier

---
+++ ViewsAndModifiers/ViewsAndModifiers/CustomModifiers.swift
@@ -0,0 +1,24 @@
+//
+//  CustomModifiers.swift
+//  ViewsAndModifiers
+//
+//  Created by OLEKSANDR ISAIEV on 11.11.2023.
+//
+
+import SwiftUI
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        
+    }
+}
+
+struct CustomModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomModifiers()
+}
